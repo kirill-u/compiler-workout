0ceda31bf2db74fbaa63bddfcbf7176f52ad99d9
ku <qwertypalmo@gmail.com>
2019-02-21 14:41:01 +0300

    implement eval for hw1

--- src/Expr.ml
+++ src/Expr.ml
@@ -36,13 +36,13 @@ let update x v s = fun y -> if x = y then v else s y
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
 (* Some testing; comment this definition out when submitting the solution. *)
-let _ =
+(*let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
-
+*)
 (* Expression evaluator
 
      val eval : state -> expr -> int
@@ -50,5 +50,27 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+
+let int_of_bool bolValue = if bolValue then 1 else 0
+
+let bool_of_int intValue = intValue != 0
+
+let eval_operator operator left right = match operator with
+  | "!!" -> int_of_bool (bool_of_int left || bool_of_int right)
+  | "&&" -> int_of_bool (bool_of_int left && bool_of_int right)
+  | "==" -> int_of_bool (left = right)
+  | "!=" -> int_of_bool (left != right)
+  | "<=" -> int_of_bool (left <= right)
+  | "<"  -> int_of_bool (left < right)
+  | ">=" -> int_of_bool (left >= right)
+  | ">"  -> int_of_bool (left > right)
+  | "+"  -> left + right
+  | "-"  -> left - right
+  | "*"  -> left * right
+  | "/"  -> left / right
+  | "%"  -> left mod right
+
+let rec eval state expr = match expr with
+  | Const v -> v
+  | Var x -> state x
+  | Binop (operator, left, right) -> eval_operator operator (eval state left) (eval state right)
