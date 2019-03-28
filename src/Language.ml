177cea2f05dabf757b64e9df48c78cce62b83f9f
ku <qwertypalmO@gmail.com>
2019-03-28 17:05:31 +0300

    hw5

--- src/Language.ml
+++ src/Language.ml
@@ -1,6 +1,11 @@
 (* Opening a library for generic programming (https://github.com/dboulytchev/GT).
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
+let counter = ref 0;;
+
+let next_var() = let result = "__repeat_variable__" ^ string_of_int !counter in
+                 counter := !counter + 1;
+                 result;;
 open GT
 
 (* Opening a library for combinator-based syntax analysis *)
@@ -43,8 +48,30 @@ module Expr =
  
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
-    *)                                                       
-    let eval st expr = failwith "Not yet implemented"
+    *)
+    let bool_of_int intValue = intValue != 0
+
+    let int_of_bool bolValue = if bolValue then 1 else 0
+
+    let eval_operator operator left right = match operator with
+      | "!!" -> int_of_bool (bool_of_int left || bool_of_int right)
+      | "&&" -> int_of_bool (bool_of_int left && bool_of_int right)
+      | "==" -> int_of_bool (left = right)
+      | "!=" -> int_of_bool (left != right)
+      | "<=" -> int_of_bool (left <= right)
+      | "<"  -> int_of_bool (left < right)
+      | ">=" -> int_of_bool (left >= right)
+      | ">"  -> int_of_bool (left > right)
+      | "+"  -> left + right
+      | "-"  -> left - right
+      | "*"  -> left * right
+      | "/"  -> left / right
+      | "%"  -> left mod right
+
+    let rec eval state expr = match expr with
+      | Const v -> v
+      | Var x -> state x
+      | Binop (operator, left, right) -> eval_operator operator (eval state left) (eval state right)
 
     (* Expression parser. You can use the following terminals:
 
@@ -52,8 +79,26 @@ module Expr =
          DECIMAL --- a decimal constant [0-9]+ as a string
                                                                                                                   
     *)
-    ostap (                                      
-      parse: empty {failwith "Not yet implemented"}
+    let parse_binop operator = ostap(- $(operator)), (fun x y -> Binop (operator, x, y))
+    ostap (
+      expr:
+        !(Ostap.Util.expr
+          (fun x -> x)
+          (Array.map (fun (a, operator) -> a, List.map parse_binop operator)
+            [|
+              `Lefta, ["!!"];
+              `Lefta, ["&&"];
+              `Nona , ["=="; "!="; "<="; "<"; ">="; ">"];
+              `Lefta, ["+"; "-"];
+              `Lefta, ["*"; "/"; "%"];
+            |]
+          )
+          primary
+          );
+      primary:
+          c: DECIMAL {Const c}
+        | x: IDENT {Var x}
+        | -"(" expr -")"
     )
     
   end
@@ -82,11 +127,58 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let rec eval conf stmt = failwith "Not yet implemented"
-                               
+
+    let rec eval c op =
+     let (st, input, output) = c in
+     match op with
+        | Read var	-> (match input with
+                | x::rest -> (Expr.update var x st), rest, output
+                | [] -> failwith("input error")
+              )
+        | Assign (var, exp) -> (Expr.update var (Expr.eval st exp) st), input, output
+        | Seq (f, s)       -> eval (eval c f) s
+        | Write exp      -> st, input, (output @ [Expr.eval st exp])
+        | Skip                              -> (st, input, output)
+        | If (e, thenStmt, elseStmt)        -> eval (st, input, output) (if Expr.bool_of_int (Expr.eval st e) then thenStmt else elseStmt)
+        | While (e, wStmt)                  -> if Expr.bool_of_int (Expr.eval st e) then eval (eval (st, input, output) wStmt) op else (st, input, output)
+        | RepeatUntil (ruStmt, e)           -> let (sNew, iNew, oNew) = eval (st, input, output) ruStmt in
+                                               if not (Expr.bool_of_int (Expr.eval sNew e)) then eval (sNew, iNew, oNew) op else (sNew, iNew, oNew)
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not yet implemented"}
+        simple:
+              "read" "(" x:IDENT ")"         {Read x}
+            | "write" "(" e:!(Expr.expr) ")" {Write e}
+            | x:IDENT ":=" e:!(Expr.expr)    {Assign (x, e)};
+        ifStmt:
+            "if" e:!(Expr.expr) "then" thenBody:parse
+        elifBranches: (%"elif" elifE:!(Expr.expr) %"then" elifBody:!(parse))*
+        elseBranch: (%"else" elseBody:!(parse))?
+        "fi" {
+               let elseBranch' = match elseBranch with
+                 | Some x -> x
+                     | None   -> Skip in
+               let expandedElseBody = List.fold_right (fun (e', body') else' -> If (e', body', else')) elifBranches elseBranch' in
+               If (e, thenBody, expandedElseBody)
+             };
+        whileStmt:
+            "while" e:!(Expr.expr) "do" body:parse "od" {While (e, body)};
+        forStmt:
+            "for" initStmt:stmt "," whileCond:!(Expr.expr) "," forStmt:stmt
+            "do" body:parse "od" {Seq (initStmt, While (whileCond, Seq (body, forStmt)))};
+        repeatUntilStmt:
+            "repeat" body:parse "until" e:!(Expr.expr) {RepeatUntil (body, e)};
+        control:
+              ifStmt
+            | whileStmt
+            | forStmt
+            | repeatUntilStmt
+            | "skip" {Skip};
+        stmt:
+              simple
+            | control;
+        parse:
+              stmt1:stmt ";" rest:parse {Seq (stmt1, rest)}
+            | stmt
     )
       
   end
--- src/SM.ml
+++ src/SM.ml
@@ -1,6 +1,6 @@
 open GT       
 open Language
-       
+open List
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
@@ -27,8 +27,37 @@ type config = int list * Stmt.config
 
    Takes an environment, a configuration and a program, and returns a configuration as a result. The
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
-*)                         
-let rec eval env conf prog = failwith "Not yet implemented"
+*)
+let eval_inter (stack, (s, inp, out)) instruction = match instruction with
+  | READ     -> (match inp with
+    | z :: tail -> (z :: stack, (s, tail, out))
+    | _         -> failwith "Not enough elements in input")
+  | WRITE    -> (match stack with
+    | z :: tail -> (tail, (s, inp, out @ [z]))
+    | _         -> failwith "Not enough elements in stack")
+  | BINOP op -> (match stack with
+    | y :: x :: tail -> ((Language.Expr.eval_operator op x y) :: tail, (s, inp, out))
+    | _              -> failwith "Not enough elements in stack")
+  | CONST z  -> (z :: stack, (s, inp, out))
+  | ST x     -> (match stack with
+    | z :: tail -> (tail, (Language.Expr.update x z s, inp, out))
+    | _         -> failwith "Not enough elements in stack")
+  | LABEL l  ->  (stack, (s, inp, out))
+  | LD x     -> ((s x) :: stack, (s, inp, out))
+
+ let rec eval env cfg p = match p with
+  | instr::tail -> (match instr with
+    | LABEL l        -> eval env cfg tail
+    | JMP l          -> eval env cfg (env#labeled l)
+    | CJMP (znz, l)  -> (let (st, rem) = cfg in match znz with
+                          | "z"  -> (match st with
+                                    | z::st' -> if z <> 0 then (eval env (st', rem) tail) else (eval env (st', rem) (env#labeled l))
+                                    | []     -> failwith "CJMP with empty stack")
+                          | "nz" -> (match st with
+                                    | z::st' -> if z <> 0 then (eval env (st', rem) (env#labeled l)) else (eval env (st', rem) tail)
+                                    | []     -> failwith "CJMP with empty stack"))
+    | _              -> eval env (eval_inter cfg instr) tail)
+  | []          -> cfg
 
 (* Top-level evaluation
 
@@ -53,4 +82,43 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile p = failwith "Not yet implemented"
+let labelGen = object
+   val mutable freeLabel = 0
+   method get = freeLabel <- freeLabel + 1; "L" ^ string_of_int freeLabel
+end
+
+ let rec compile_with_labels p lastL =
+  let rec expr = function
+  | Expr.Var   x          -> [LD x]
+  | Expr.Const n          -> [CONST n]
+  | Expr.Binop (op, x, y) -> expr x @ expr y @ [BINOP op]
+  in match p with
+  | Stmt.Seq (s1, s2)  -> (let newLabel = labelGen#get in
+                           let (compiled1, used1) = compile_with_labels s1 newLabel in
+                           let (compiled2, used2) = compile_with_labels s2 lastL in
+                           (compiled1 @ (if used1 then [LABEL newLabel] else []) @ compiled2), used2)
+  | Stmt.Read x        -> [READ; ST x], false
+  | Stmt.Write e       -> (expr e @ [WRITE]), false
+  | Stmt.Assign (x, e) -> (expr e @ [ST x]), false
+  | Stmt.If (e, s1, s2) ->
+    let lElse = labelGen#get in
+    let (compiledS1, used1) = compile_with_labels s1 lastL in
+    let (compiledS2, used2) = compile_with_labels s2 lastL in
+    (expr e @ [CJMP ("z", lElse)]
+    @ compiledS1 @ (if used1 then [] else [JMP lastL]) @ [LABEL lElse]
+    @ compiledS2 @ (if used2 then [] else [JMP lastL])), true
+  | Stmt.While (e, body) ->
+    let lCheck = labelGen#get in
+    let lLoop = labelGen#get in
+    let (doBody, _) = compile_with_labels body lCheck in
+    ([JMP lCheck; LABEL lLoop] @ doBody @ [LABEL lCheck] @ expr e @ [CJMP ("nz", lLoop)]), false
+  | Stmt.RepeatUntil (body, e) ->
+    let lLoop = labelGen#get in
+    let (repeatBody, _) = compile_with_labels body lastL in
+    ([LABEL lLoop] @ repeatBody @ expr e @ [CJMP ("z", lLoop)]), false
+  | Stmt.Skip -> [], false
+
+ let rec compile p =
+  let label = labelGen#get in
+  let compiled, used = compile_with_labels p label in
+  compiled @ (if used then [LABEL label] else [])
